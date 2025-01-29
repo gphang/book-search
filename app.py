c77add928ec59629cfbd68ad6f3cb4d607fd6fc4
gphang <gphang@umich.edu>
2025-01-29 12:40:46 +0000

    fixed app.py port

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def submit():
     return render_template("results.html", genre=genre_query, books=books)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="8080", debug=True)
